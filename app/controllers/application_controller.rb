794b81900d138e9366fa6bfa7adfe005ca09ab4d
IvanMonroy <ivmonroy4@poligran.edu.co>
2023-06-11 00:08:19 -0500

    Cambiando application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   def authenticate_user!
     return true
